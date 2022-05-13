b1ffbccd82097116f97379bfdc5f79805bb60faf
JackWang <jack4shihong@gmail.com>
2022-05-13 13:11:50 +0800

    enhanced DatasetPath.PascalVOC2012

--- helper.py
+++ helper.py
@@ -21,7 +21,7 @@ class ProjectPath:
     runs: Path = base.joinpath("runs")
     log: Path = base.joinpath("logs")
     config: Path = base.joinpath("config")
-    dataset: Path = base.joinpath("dataset")
+    dataset: Path = base.joinpath("datasets")
     checkpoints: Path = base.joinpath("checkpoints")
 
     def __init__(self) -> None:
@@ -30,55 +30,87 @@ class ProjectPath:
                 project_path.mkdir(parents=True, exist_ok=True)
 
 
+PascalClassificationType = Dict[str, List[Path]]
+PascalSegmentationType = List[Path]
+PascalType = TypeVar("PascalType", PascalClassificationType, PascalSegmentationType)
+
+
 class DatasetPath:
     base: Path = ProjectPath.dataset
 
     class Cifar10:
-        base: Path = ProjectPath.base.joinpath("dataset/cifar-10")
+        base: Path = ProjectPath.base.joinpath("datasets/cifar-10")
         meta: Path = base.joinpath("batches.meta")
         test: Path = base.joinpath("test_batch")
         train: List[Path] = list(base.glob("data*"))
 
     class Cifar100:
-        base: Path = ProjectPath.base.joinpath("dataset/cifar-100")
+        base: Path = ProjectPath.base.joinpath("datasets/cifar-100")
         meta: Path = base.joinpath("meta")
         test: Path = base.joinpath("test")
         train: Path = base.joinpath("train")
 
     class PascalVOC2012:
-        base: Path = ProjectPath.base.joinpath("dataset/PascalVOC2012")
+        base: Path = ProjectPath.base.joinpath("datasets/PascalVOC2012").resolve()
         JPEGImages: Path = base.joinpath("JPEGImages")
+        ImageSets: Path = base.joinpath("ImageSets")
+        Annotation: Path = base.joinpath("Annotation")
+        SegmentationClass: Path = base.joinpath("SegmentationClass")
+        SegmentationObject: Path = base.joinpath("SegmentationObject")
 
-        _train_class_idx: List[Path] = list(base.joinpath("ImageSets", "Main").glob(r"*_train.txt"))
-        _val_class_idx: List[Path] = list(base.joinpath("ImageSets", "Main").glob(r"*_val.txt"))
+        train: PascaType
+        val: PascalType
 
-        # Attention: 同一个图像是存在多个标签的, 后续需要去重复
-        # get train
-        train_idx: Dict[str, List[Path]] = {}
-        _seen_img = np.array(["-1"], dtype=str)
-        for _path in _train_class_idx:
-            _cls = _path.stem.split("_")[0]
-            _data = np.loadtxt(_path, dtype=str)
-            _mask = np.where(_data == "1")[0]
-            train_idx[_cls] = np.apply_along_axis(
-                arr=_data[_mask], axis=1,
-                func1d=lambda x: ProjectPath.base.joinpath(
-                    "dataset", "PascalVOC2012", "JPEGImages", f"{x[0]}.jpg"
-                )
-            ).tolist()
+        @classmethod
+        def classification(cls) -> "PascalVOC2012":
+            # Attention: 同一个图像是存在多个标签的, 后续需要去重复
+            train_idx: Dict[str, List[Path]] = {}
+            train_class_idx: List[Path] = list(cls.ImageSets.joinpath("Main").glob(r"*_train.txt"))
 
-        # get validation
-        val_idx: Dict[str, List[Path]] = {}
-        for _path in _val_class_idx:
-            _cls = _path.stem.split("_")[0]
-            _data = np.loadtxt(_path, dtype=str)
-            _mask = np.where(_data == "1")[0]
-            val_idx[_cls] = np.apply_along_axis(
-                arr=_data[_mask], axis=1,
-                func1d=lambda x: ProjectPath.base.joinpath(
-                    "dataset", "PascalVOC2012", "JPEGImages", f"{x[0]}.jpg"
-                )
-            ).tolist()
+            # get train
+            seen_img = np.array(["-1"], dtype=str)
+            for path in train_class_idx:
+                clss = path.stem.split("_")[0]
+                data = np.loadtxt(path, dtype=str)
+                mask = np.where(data == "1")[0]
+                train_idx[clss] = np.apply_along_axis(
+                    arr=data[mask], axis=1,
+                    func1d=lambda x: cls.JPEGImages.joinpath(f"{x[0]}.jpg")
+                ).tolist()
+
+            # get validation
+            val_idx: Dict[str, List[Path]] = {}
+            val_class_idx: List[Path] = list(cls.ImageSets.joinpath("Main").glob(r"*_val.txt"))
+            for path in val_class_idx:
+                clss = path.stem.split("_")[0]
+                data = np.loadtxt(path, dtype=str)
+                mask = np.where(data == "1")[0]
+                val_idx[clss] = np.apply_along_axis(
+                    arr=data[mask], axis=1,
+                    func1d=lambda x: cls.JPEGImages.joinpath(f"{x[0]}.jpg")
+                ).tolist()
+
+            # add to class property
+            cls.train: PascalClassificationType = train_idx
+            cls.val: PascalClassificationType = val_idx
+            return cls
+
+        @classmethod
+        def segmentation(cls) -> "PascalVOC2012":
+            seg_path = cls.ImageSets.joinpath("Segmentation")
+            train = np.loadtxt(seg_path.joinpath("train.txt"), dtype=str)[:, np.newaxis]
+            val = np.loadtxt(seg_path.joinpath("val.txt"), dtype=str)[:, np.newaxis]
+
+            cls.train: PascalSegmentationType = np.apply_along_axis(
+                arr=train, axis=1,
+                func1d=lambda x: cls.SegmentationClass.joinpath(f"{x[0]}.png")
+            )
+            cls.val: PascalSegmentationType = np.apply_along_axis(
+                arr=val, axis=1,
+                func1d=lambda x: cls.SegmentationClass.joinpath(f"{x[0]}.png")
+            )
+
+            return cls
 
 
 class ClassLabelLookuper:
@@ -134,7 +166,7 @@ class ClassificationEvaluator:
         k = (y >= 0) & (y < len(self.cls))
 
         # get top 5 predictions
-        # Warn 下面这句话计算的有问题, 直接使用torch.topk计算
+        # Warn: 下面这句话计算的有问题, 直接使用torch.topk计算
         # y_pred = np.argpartition(y_pred, kth=-5, axis=1)[:, -5:][:, ::-1]
 
         # get top 1 confusion matrix of the batch
@@ -416,7 +448,12 @@ if __name__ == "__main__":
     # image = [np.random.randint(low=0, high=256, size=(*rand_shape(), 3), dtype=int) for i in range(64)]
     # visualize_plt(image)
 
-    ce = ClassificationEvaluator(dataset="Cifar100")
-    ce.confusion_top1 += 10
-    a = ce.get_confusion()
-    print(a)
+    # ce = ClassificationEvaluator(dataset="Cifar100")
+    # ce.confusion_top1 += 10
+    # a = ce.get_confusion()
+    # print(a)
+
+    # print(DatasetPath.PascalVOC2012.classification().train)
+    for i in DatasetPath.PascalVOC2012.segmentation().train:
+        print(i, i.exists())
+    # print(DatasetPath.PascalVOC2012.segmentation().train)
